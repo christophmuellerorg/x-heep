786af747fc9d614de60d507344744bdef7a7f9a6
Christoph Müller <christoph@christophmueller.org>
2024-10-03 16:07:50 +0200

    oss cad suite installer embedding xheep requirements

--- scripts/install_oss_cad_suite.sh
+++ scripts/install_oss_cad_suite.sh
@@ -5,12 +5,14 @@ arch=$(uname -m)
 release_date=2024-09-28
 
 if [ $os == "Linux" ]; then
+    sed=sed
     if [ $arch == "arm64" ]; then
         release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-linux-arm64-${release_date//-/}.tgz
     elif [ $arch == "x86_64" ]; then 
         release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-linux-x64-${release_date//-/}.tgz
     fi
 elif [ $os  == "Darwin" ]; then
+    sed=gsed
     if [ $arch == "arm64" ]; then
         release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-darwin-arm64-${release_date//-/}.tgz
     elif [ $arch == "x86_64" ]; then 
@@ -24,3 +26,9 @@ echo Downloading $release
 wget $release -O oss-cad-suite.tgz
 mkdir -p tools
 tar -xzvf oss-cad-suite.tgz -C tools
+
+echo Adding X-Heep requirements into the OSS cad suite python venv
+source tools/oss-cad-suite/environment
+tabbypy3 -m pip install --upgrade pip
+pip3 install -r python-requirements.txt
+$sed -i 's/OSS CAD Suite/XHEEP OSS CAD Suite/g' tools/oss-cad-suite/environment
