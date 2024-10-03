83487c63e360f3c3d843b4bf62e19466552a89fb
Christoph Müller <christoph@christophmueller.org>
2024-10-03 14:41:55 +0200

    Script to download oss cad suite

--- scripts/install_oss_cad_suite.sh
+++ scripts/install_oss_cad_suite.sh
@@ -4,17 +4,16 @@ os=$(uname -s)
 arch=$(uname -m)
 release_date=2024-09-28
 
-# Default: Linux/x64
-release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-linux-x64-${release_date//-/}.tgz
-
-if [ $os  =~ ^Linux ]; then
-    if [ $arch =~ ^arm64 ]; then
+if [ $os == "Linux" ]; then
+    if [ $arch == "arm64" ]; then
         release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-linux-arm64-${release_date//-/}.tgz
+    elif [ $arch == "x86_64" ]; then 
+        release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-linux-x64-${release_date//-/}.tgz
     fi
-elif [ $os  =~ ^Darwin ]; then
-    if [ $arch =~ ^arm64 ]; then
+elif [ $os  == "Darwin" ]; then
+    if [ $arch == "arm64" ]; then
         release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-darwin-arm64-${release_date//-/}.tgz
-    elif [ $arch =~ ^x86_64 ]; then 
+    elif [ $arch == "x86_64" ]; then 
         release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-darwin-x64-${release_date//-/}.tgz
     fi 
 fi
