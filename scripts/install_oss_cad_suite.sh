1a98bd330ea57b1b9356851904213cffede67b87
Christoph Müller <christoph@christophmueller.org>
2024-10-03 14:37:09 +0200

    Script to download oss cad suite

--- scripts/install_oss_cad_suite.sh
+++ scripts/install_oss_cad_suite.sh
@@ -1,22 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 os=$(uname -s)
 arch=$(uname -m)
 release_date=2024-09-28
-release_date_nodash=$(echo $release_date | sed s/-//g)
 
 # Default: Linux/x64
-release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-linux-x64-${release_date_nodassh}.tgz
+release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-linux-x64-${release_date//-/}.tgz
 
 if [ $os  =~ ^Linux ]; then
     if [ $arch =~ ^arm64 ]; then
-        release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-linux-arm64-${release_date_nodassh}.tgz
+        release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-linux-arm64-${release_date//-/}.tgz
     fi
 elif [ $os  =~ ^Darwin ]; then
     if [ $arch =~ ^arm64 ]; then
-        release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-darwin-arm64-${release_date_nodassh}.tgz
+        release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-darwin-arm64-${release_date//-/}.tgz
     elif [ $arch =~ ^x86_64 ]; then 
-        release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-darwin-x64-${release_date_nodassh}.tgz
+        release=https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${release_date}/oss-cad-suite-darwin-x64-${release_date//-/}.tgz
     fi 
 fi
 
